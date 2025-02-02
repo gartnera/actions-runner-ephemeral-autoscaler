964a586c1c425ea7412324591f8ef47e71087763
Alex Gartner <git@agartner.com>
2025-02-02 15:09:38 -0800

    Add latest runner version detection (#11)

--- providers/common/cloudinit.go
+++ providers/common/cloudinit.go
@@ -1,12 +1,26 @@
 package common
 
 import (
+	"context"
 	_ "embed"
 	"strings"
+
+	"github.com/google/go-github/v68/github"
 )
 
 //go:embed cloud-init-prepare.yml
-var CloudInitPrepare string
+var cloudInitPrepare string
+
+func GetCloudInitPrepare(ctx context.Context) (string, error) {
+	client := github.NewClient(nil)
+	release, _, err := client.Repositories.GetLatestRelease(ctx, "actions", "runner")
+	if err != nil {
+		return "", err
+	}
+	runnerVersion := strings.TrimPrefix(release.GetTagName(), "v")
+	conf := strings.ReplaceAll(cloudInitPrepare, "{{RUNNER_VERSION}}", runnerVersion)
+	return conf, nil
+}
 
 //go:embed cloud-init-start.yml
 var cloudInitStartTemplate string
