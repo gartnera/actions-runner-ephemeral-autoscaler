bdb2882924deb70d2ea560d6f8a4317eab173f34
Alex Gartner <git@agartner.com>
2025-02-15 14:07:27 -0800

    Make provider generic (#15)

--- cmd/actions-runner-ephemeral-autoscaler/main.go
+++ cmd/actions-runner-ephemeral-autoscaler/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/gartnera/actions-runner-ephemeral-autoscaler/autoscaler"
+	"github.com/gartnera/actions-runner-ephemeral-autoscaler/providers/githubtoken"
 	"github.com/gartnera/actions-runner-ephemeral-autoscaler/providers/interfaces"
 	"github.com/gartnera/actions-runner-ephemeral-autoscaler/providers/lxd"
 	"github.com/google/go-github/v68/github"
@@ -16,36 +17,27 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type ActionsRepoTokenProvider struct {
-	Client *github.Client
-	Org    string
-	Repo   string
-}
-
-func (p *ActionsRepoTokenProvider) URL() string {
-	return fmt.Sprintf("https://github.com/%s/%s", p.Org, p.Repo)
-}
-
-func (p *ActionsRepoTokenProvider) Token(ctx context.Context) (string, error) {
-	tokenResponse, _, err := p.Client.Actions.CreateRegistrationToken(ctx, p.Org, p.Repo)
-	if err != nil {
-		return "", fmt.Errorf("creating registration token: %v", err)
-	}
-	return tokenResponse.GetToken(), nil
-}
-
 func main() {
 	org := flag.String("org", os.Getenv("GITHUB_ORG"), "GitHub organization name")
 	repo := flag.String("repo", os.Getenv("GITHUB_REPO"), "GitHub repository name")
 	labels := flag.String("labels", "", "Runner labels")
 	targetIdle := flag.Int("target-idle", 1, "Target number of idle runners")
 	customCloudInitPath := flag.String("custom-cloud-init", "", "Path to custom cloud init file")
+	providerName := flag.String("provider", "lxd", "Provider to use (only 'lxd' supported)")
 	flag.Parse()
 
 	if *org == "" || *repo == "" || *labels == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
+
+	if *providerName != "lxd" {
+		fmt.Printf("Invalid provider: %s. Only 'lxd' is supported\n", *providerName)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var provider interfaces.Provider
 
 	ctx := context.Background()
 	provider, err := lxd.New()
@@ -55,7 +47,7 @@ func main() {
 
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")})
 	tc := oauth2.NewClient(ctx, ts)
-	tokenProvider := &ActionsRepoTokenProvider{
+	tokenProvider := &githubtoken.RepoProvider{
 		Client: github.NewClient(tc),
 		Org:    *org,
 		Repo:   *repo,
